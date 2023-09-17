bb70481bc3bfc9413ed42207d54d5af74c5f3516
Vishnu <vishnukun954AV@protonmail.com>
2023-09-17 08:40:58 +0530

    Added test for place_piece method, corrections in print_board test

--- spec/chessboard_spec.rb
+++ spec/chessboard_spec.rb
@@ -7,30 +7,44 @@ require 'spec_helper'
 
 describe Chessboard do
   describe '#print_board' do
-    subject(game_board) { described_class.new }
+    subject(:game_board) { described_class.new }
     context 'when invoked' do
       it 'prints the board' do
         result = game_board.print_board
         solution = <<-Board
                        ⒜   ⒝   ⒞   ⒟   ⒠   ⒡   ⒢   ⒣
-                  ⑴ | ♖ | ♘ | ♗ | ♕ | ♔ | ♗ | ♘ | ♖ | ⑴
+                 ⑴  | ♖ | ♘ | ♗ | ♕ | ♔ | ♗ | ♘ | ♖ |  ⑴
                   ⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋
-                  ⑵ | ♙ | ♙ | ♙ | ♙ | ♙ | ♙ | ♙ | ♙ | ⑵
+                 ⑵  | ♙ | ♙ | ♙ | ♙ | ♙ | ♙ | ♙ | ♙ |  ⑵
                  ⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋
-              ⑶ | ░░ | ██ | ░░ | ██ | ░░ | ██ | ░░ | ██ | ░░ | ⑶
-              ⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋
-              ⑷ | ██ | ░░ | ██ | ░░ | ██ | ░░ | ██ | ░░ | ██ | ⑷
-              ⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋
-              ⑸ | ░░ | ██ | ░░ | ██ | ░░ | ██ | ░░ | ██ | ░░ | ⑸
-              ⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋
-              ⑹ | ██ | ░░ | ██ | ░░ | ██ | ░░ | ██ | ░░ | ██ | ⑹
+                 ⑶ | ░░ | ██ | ░░ | ██ | ░░ | ██ | ░░ | ██ | ⑶
                  ⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋
-                  ⑺ | ♟ | ♟ | ♟ | ♟ | ♟ | ♟ | ♟ | ♟ | ⑺
-                  ⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋
-                  ⑻ | ♜ | ♞ | ♝ | ♚ | ♛ | ♝ | ♞ | ♜ | ⑻
+                 ⑷ | ██ | ░░ | ██ | ░░ | ██ | ░░ | ██ | ░░ | ⑷
+                 ⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋
+                 ⑸ | ░░ | ██ | ░░ | ██ | ░░ | ██ | ░░ | ██ | ⑸
+                 ⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋
+                 ⑹ | ██ | ░░ | ██ | ░░ | ██ | ░░ | ██ | ░░ | ⑹
+                 ⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋
+                 ⑺  | ♟ | ♟ | ♟ | ♟ | ♟ | ♟ | ♟ | ♟ |  ⑺
+                  ⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋
+                 ⑻  | ♜ | ♞ | ♝ | ♚ | ♛ | ♝ | ♞ | ♜ |  ⑻
                        ⒜   ⒝   ⒞   ⒟   ⒠   ⒡   ⒢   ⒣
         Board
         expect(result).to eq(solution)
+      end
+    end
+  end
+
+  describe "#place_piece" do
+    subject(:game_place) { described_class.new }
+    context 'when given piece and position' do 
+      before do 
+        board = game_place.instance_variable_get(:@board)
+        allow(game_place).to receive(:coordinate?).with('a5').and_return(board[4][0])
+      end
+      it 'should place the piece on the board' do
+        game_place.place_piece('♜', 'a5')
+        expect(game_place.instance_variable_get(:@board)[4][0]).to eq('♜')
       end
     end
   end
