e023f76f914a87e0c714ea1cc426c08ffaf58d21
Vishnu <vishnukun954AV@protonmail.com>
2023-09-19 16:09:20 +0530

    Added tests for the Pawn class

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -2,63 +2,93 @@
 
 # spec/pieces_spec.rb
 
-require 'pieces'
 require 'spec_helper'
+require 'pieces'
 
 describe Pawn do
-  subject(:pawn) { described_class.new(:white) }
 
-  before do 
-    allow(pawn).to receive(:valid_move?).and_return(true)
-    allow(pawn).to receive(:in_bounds?).and_return(true)
+  describe "#legal_moves" do
+    subject(:pawn_moves) { described_class.new }
+
+    context "when pawn is at starting position" do
+      it "returns single and double step moves" do
+        expect(pawn_moves.legal_moves('a7')).to contain_exactly('a6', 'a5')
+      end
+    end
+
+    context "when pawn blockage occurs" do
+      before do 
+        allow(pawn_moves).to receive(:pawn_blocked?).and_return(true)
+      end
+      it "returns 0" do
+        expect(pawn_moves.legal_moves('a4')).to contain_exactly(0)
+      end
+    end
   end
 
-  describe '#possible_moves?' do
-    context "when pawn is at starting position i.e. 'a7'" do
+  describe "#capture" do 
+    subject(:pawn_captures) { described_class.new }
+    board = double("Board")
+    context "when capturing an opponent's piece" do
       before do 
-        allow(pawn).to receive(:occupied?).and_return(false)
-        allow(pawn).to receive(:capture_move?).and_return(false)
-        allow(pawn).to receive(:en_passant?).and_return(false)
+        enemy_pawn = described_class.new('d5')
+        allow(board).to receive(:remove_piece)
+        allow(board).to receive(:get_piece).with('d5').and_return(enemy_pawn)
       end
-      it 'returns correct possible moves' do
-        result = pawn.possible_moves?('a7')
-        expect(result).to contain_exactly(['a5', 'a6'])
-      end
-    end
-    
-    context "when pawn has already moved" do
-      before do
-        allow(pawn).to receive(:occupied?).and_return(false)
-        allow(pawn).to receive(:capture_move?).and_return(false)
-        allow(pawn).to receive(:en_passant?).and_return(false)
-      end
-      it "returns correct possible moves" do
-        result = pawn.possible_moves?('a6')
-        expect(result).to contain_exactly(['a5'])
+      it "removes the captured piece from the board" do 
+        pawn_captures.capture('d5', board)
+        expect(board).to have_received(:remove_piece).with('d5')
       end
     end
-    
-    context "when pawn is face to face with other pawns" do
-      before do
-        allow(pawn).to receive(:occupied?).and_return(true)
-        allow(pawn).to receive(:capture_move?).and_return(false)
-        allow(pawn).to receive(:en_passant?).and_return(false)
-      end
-      it "returns 0" do 
-        result = pawn.possible_moves?('b4')
-        expect(result).to eq(0)
+  end
+
+  describe "#move" do 
+    subject(:pawn_moves) { described_class.new('a7') }
+    context "when invoked with 'c6' to 'c5'" do 
+      it "should move the pawn to 'c5' spot" do 
+        pawn_moves.move('c5')
+        expect(pawn_moves.locate).to eq('c5')
       end
     end
-    
-    context "when pawn can capture opponent's pawn" do 
-      before do
-        allow(pawn).to receive(:occupied?).and_return(true)
-        allow(pawn).to receive(:capture_move?).and_return(true)
-        allow(pawn).to receive(:en_passant?).and_return(true)
+
+    context "in case of pawn's initial double-step" do 
+      before do 
+        allow(pawn_moves).to receive(:double_step_possible?).and_return(true)
       end
-      it "returns correct possible moves" do 
-        result = pawn.possible_moves?('d4')
-        expect(result).to contain_exactly(['c3', 'e3'])
+      it "should move the pawn from 'a7' to 'a5'" do
+        pawn_moves.move('a5')
+        expect(pawn_moves.locate).to eq('a5')
+      end
+    end
+
+    context "when invalid move is given" do 
+      it "should raise an error" do
+        error_message = "Invalid move. Please try again."
+        expect { pawn_moves.move('g0') }.to raise_error(error_message)
+      end
+    end
+  end
+
+  describe "#en_passant" do 
+    subject(:pawn_en_passant) { described_class.new }
+    
+    context "when en-passant is possible" do
+      before do 
+        allow(pawn_en_passant).to receive(:en_passant_possible?).and_return(true)
+      end
+      it "should capture and place the pawn correctly" do
+        pawn_en_passant.en_passant('b3')
+        expect(pawn_en_passant.locate).to eq('b3')
+      end
+    end
+  end
+
+  describe "#promote" do 
+    subject(:pawn_promoted) { described_class.new }
+    context "when promoting to queen" do 
+      it "should promote the pawn to queen" do
+        pawn_promoted.promote('queen')
+        expect(pawn_promoted).to be_an_instance_of(Queen)
       end
     end
   end
