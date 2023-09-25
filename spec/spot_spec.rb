efa315609a8f0acccaaf50106e542baba18abfa1
Vishnu <vishnukun954AV@protonmail.com>
2023-09-25 14:02:04 +0530

    Initialized tests

---
+++ spec/spot_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# spec/spot_spec.rb
+
+require 'spec_helper'
+
+describe Spot do 
+  
+  describe "#is_empty?" do
+  end
+
+  describe "#get_piece" do
+  end
+
+  describe "#set_piece" do
+  end
+
+  describe "#remove_piece" do
+  end
+end
