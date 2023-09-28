9ed92eaaac208389ec50e9e3f0781a9dd9ec9217
Vishnu <vishnukun954AV@protonmail.com>
2023-09-28 19:18:44 +0530

    Typos and refactored contexts

--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -8,10 +8,11 @@ require 'chessboard'
 
 describe King do
   describe '#valid_moves' do
-    context 'when invoked with given position and piece' do
+    context 'when king is at (0, 3) on empty chess board' do
+      subject(:black_king) { described_class.new(-5) }
+      subject(:chess_board) { Chessboard.new }
+
       before do
-        subject(:black_king) { described_class.new(-5) }
-        subject(:chess_board) { Chessboard.new }
         chess_board.add_piece(black_king, 0, 3)
       end
       it 'returns correct number of moves' do
@@ -19,10 +20,10 @@ describe King do
       end
     end
 
-    context 'when piece has moved' do
+    context 'when king has moved' do
+      subject(:black_king) { described_class.new(-5) }
+      subject(:chess_board) { Chessboard.new }
       before do
-        subject(:black_king) { described_class.new(-5) }
-        subject(:chess_board) { Chessboard.new }
         chess_board.add_piece(black_king, 3, 4)
       end
       it 'returns correct number of moves' do
@@ -33,10 +34,11 @@ describe King do
   end
 
   describe '#move_to' do
-    context 'when given a move' do
+    context 'when king is at (0, 4) on empty chess board' do
+      subject(:king) { described_class.new(6) }
+      subject(:chess_board) { Chessboard.new }
+
       before do
-        subject(:king) { described_class.new(6) }
-        subject(:chess_board) { Chessboard.new }
         chess_board.add_piece(king, 0, 4)
       end
       it 'moves the piece to the correct position' do
@@ -47,11 +49,12 @@ describe King do
   end
 
   describe 'capture_at' do
-    context 'when given position' do
+    context 'when king is at position (1, 4) on the chess board with pawn at (2, 5)' do
+      subject(:king) { described_class.new(-5) }
+      subject(:chess_board) { Chessboard.new }
+      subject(:pawn) { Pawn.new(1) }
+
       before do
-        subject(:king) { described_class.new(-5) }
-        subject(:chess_board) { Chessboard.new }
-        subject(:pawn) { Pawn.new(1) }
         chess_board.add_piece(king, 1, 4)
         chess_board.add_piece(pawn, 2, 5)
       end
@@ -61,42 +64,48 @@ describe King do
       end
     end
   end
-  
+
   describe '#castle' do
-    context 'when available' do
+    context 'when castling is possible' do
+      subject(:chess_board) { Chessboard.new }
+      subject(:white_king) { described_class.new(6) }
+      subject(:left_rook) { Rook.new(4) }
+      subject(:right_rook) { Rook.new(4) }
+
       before do
-        subject(:chess_board) { Chessboard.new }
-        subject(:white_king) { described_class.new(6) }
-        subject(:left_rook) { Rook.new(4) }
-        subject(:right_rook) { Rook.new(4) }
         chess_board.add_piece(white_king, 7, 4)
         chess_board.add_piece(left_rook, 7, 0)
         chess_board.add_piece(right_rook, 7, 7)
+
         allow(chess_board).to receive(:castle_possible?).and_return(true)
       end
       it 'exchanges the position of the left rook and king' do
         white_king.castle(left_rook)
+
         expect(white_king.position).to eq([7, 2])
         expect(left_rook.position).to eq([7, 3])
         expect(right_rook.position).to eq([7, 7])
       end
     end
-    
+
     context 'when a piece is inbetween' do
+      subject(:chess_board) { Chessboard.new }
+      subject(:white_king) { described_class.new(6) }
+      subject(:left_rook) { Rook.new(4) }
+      subject(:right_rook) { Rook.new(4) }
+      subject(:knight) { Knight.new(2) }
+
       before do
-        subject(:chess_board) { Chessboard.new }
-        subject(:white_king) { described_class.new(6) }
-        subject(:left_rook) { Rook.new(4) }
-        subject(:right_rook) { Rook.new(4) }
-        subject(:knight) { Knight.new(2) }
         chess_board.add_piece(white_king, 7, 4)
         chess_board.add_piece(left_rook, 7, 0)
         chess_board.add_piece(right_rook, 7, 7)
         chess_board.add_piece(knight, 7, 1)
+
         allow(chess_board).to receive(:castle_possible?).and_return(false)
       end
       it "doesn't change the positions of the rook and king" do
         white_king.castle(left_rook)
+
         expect(white_king.position).to eq([7, 4])
         expect(left_rook.position).to eq([7, 0])
         expect(right_rook.position).to eq([7, 7])
