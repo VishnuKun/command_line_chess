446773dbbc4db61d52a1e7309dfc34761193a315
Vishnu <vishnukun954AV@protonmail.com>
2023-09-29 09:41:14 +0530

    Added tests for the game_over? method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,5 +7,73 @@ require 'pieces'
 require 'player'
 require 'chessboard'
 
-describe Game do 
+describe Game do
+  describe '#game_over?' do
+    let(:game) { described_class.new }
+
+    context 'when the game is ongoing' do
+      before do
+        allow(game).to receive(:draw).and_return(false)
+        allow(game).to receive(:win?).and_return(false)
+        allow(game).to receive(:lose?).and_return(false)
+      end
+      it 'returns false' do
+        expect(game.game_over?).to eq(false)
+      end
+    end
+
+    context 'when enemy checkmates your king' do
+      before do
+        allow(game).to receive(:lose?).and_return(true)
+      end
+      it 'returns true' do
+        expect(game.game_over?).to eq(true)
+      end
+    end
+
+    context 'when you checkmates your enemies king' do
+      before do
+        allow(game).to receive(:win?).and_return(true)
+      end
+      it 'returns true' do
+        expect(game.game_over?).to eq(true)
+      end
+    end
+
+    context 'when a stalemate occurs' do
+      before do
+        allow(game).to receive(:draw?).and_return(true)
+      end
+      it 'returns true' do
+        expect(game.game_over?).to eq(true)
+      end
+    end
+
+    context '50 move rule' do
+      before do
+        allow(game).to receive(:draw?).and_return(true)
+      end
+      it 'returns true' do
+        expect(game.game_over?).to eq(true)
+      end
+    end
+
+    context 'in case of repition' do
+      before do
+        allow(game).to receive(:draw?).and_return(true)
+      end
+      it 'returns true' do
+        expect(game.game_over?).to eq(true)
+      end
+    end
+
+    context 'insufficient material' do
+      before do
+        allow(game).to receive(:draw?).and_return(true)
+      end
+      it 'returns true' do
+        expect(game.game_over?).to eq(true)
+      end
+    end
+  end
 end
