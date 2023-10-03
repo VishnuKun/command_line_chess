258ddd3b37eac6dd9341ab8b405b57649627233e
Vishnu <vishnukun954AV@protonmail.com>
2023-10-03 09:53:04 +0530

    Added create_piece method for creating instances based on integer

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -5,9 +5,30 @@
 require_relative 'chessboard'
 
 class Piece
-    attr_accessor :piece_id
+  attr_accessor :piece_id, :captured, :moved
 
-    def initialize(piece_id)
-        @piece_id = piece_id
+  def initialize(piece_id)
+    @piece_id = piece_id
+    @captured = false
+    @moved = false
+  end
+
+  def self.create_piece(piece_id)
+    case piece_id
+    when 1, -1
+      Pawn.new(piece_id)
+    when 2, -2
+      Knight.new(piece_id)
+    when 3, -3
+      Bishop.new(piece_id)
+    when 4, -4
+      Rook.new(piece_id)
+    when 5, -6
+      Queen.new(piece_id)
+    when 6, -5
+      King.new(piece_id)
+    else
+      raise ArgumentError, "Invalid piece id: #{piece_id}"
     end
+  end
 end
