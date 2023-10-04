9b0768d1b4b653f7fc0fa1658069df3eb1e9161c
Vishnu <vishnukun954AV@protonmail.com>
2023-10-04 09:51:24 +0530

    Added attributes color and symbol plus methods to set them

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -2,15 +2,23 @@
 
 # lib/pieces.rb
 
+require_relative 'pawn'
+require_relative 'knight'
+require_relative 'bishop'
+require_relative 'rook'
+require_relative 'queen'
+require_relative 'king'
 require_relative 'chessboard'
 
 class Piece
-  attr_accessor :piece_id, :captured, :moved
+  attr_accessor :piece_id, :captured, :moved, :symbol , :color
 
   def initialize(piece_id)
     @piece_id = piece_id
     @captured = false
     @moved = false
+    @symbol = set_symbol(piece_id)
+    @color = set_color(piece_id)
   end
 
   def self.create_piece(piece_id)
@@ -30,5 +38,38 @@ class Piece
     else
       raise ArgumentError, "Invalid piece id: #{piece_id}"
     end
+  end
+
+  def set_symbol(piece_id)
+    case piece_id
+    when 1
+      '♙'
+    when -1 
+      '♟'
+    when 2
+      '♘'
+    when -2
+      '♞'
+    when 3
+      '♗'
+    when -3
+      '♝'
+    when 4
+      '♖'
+    when -4
+      '♜'
+    when 5
+      '♕'
+    when -5
+      '♚'
+    when 6
+      '♔'
+    when -6
+      '♛'
+    end
+  end
+
+  def set_color(piece_id)
+    piece_id.negative? ? 'black' : 'white'
   end
 end
