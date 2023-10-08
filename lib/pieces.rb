88d61633fb35d3987413977180c3e2d6f01f9e40
Vishnu <vishnukun954AV@protonmail.com>
2023-10-08 15:14:29 +0530

    move_to now functional

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -3,8 +3,8 @@
 # lib/pieces.rb
 
 class Piece
-  attr_accessor :piece_id, :captured, :moved, :symbol , :color
-  
+  attr_accessor :piece_id, :captured, :moved, :symbol, :color
+
   def initialize(piece_id)
     @piece_id = piece_id
     @captured = false
@@ -38,7 +38,7 @@ class Piece
     case piece_id
     when 1
       '♙'
-    when -1 
+    when -1
       '♟'
     when 2
       '♘'
@@ -62,10 +62,74 @@ class Piece
       '♛'
     end
   end
-  
+
   # sets the color to the piece according to the piece id
   def set_color(piece_id)
-    piece_id.negative? ? 'black' : 'white'
+    piece_id.negative? ? 'white' : 'black'
+  end
+
+  # returns the position where piece can be placed on board
+  def valid_moves(board_array)
+    moves = []
+    current_spot = find_spot(self, board_array)
+    id = piece_id
+    case id
+    when 1, -1 # Pawn
+      moves = valid_pawn_moves(current_spot, board_array)
+    when 2, -2 # Knight
+      moves = valid_knight_moves(current_spot, board_array)
+    when 3, -3 # Bishop
+      moves = valid_bishop_moves(current_spot, board_array)
+    when 4, -4 # Rook
+      moves = valid_rook_moves(current_spot, board_array)
+    when 5, -6 # Queen
+      moves = valid_queen_moves(current_spot, board_array)
+    when 6, -5 # King
+      moves = valid_king_moves(current_spot, board_array)
+    end
+    moves
+  end
+
+  # moves the piece to the given position
+  def move_to(row, column, board)
+    current_piece = self
+    # make the current spot.piece = nil
+    current_spot = find_spot(current_piece, board)
+    current_spot.piece = nil
+    # find the destination spot
+    destination = board[row][column]
+    # check if the desination isn't inhabited by friendly piece
+    return unless destination.empty? || current_piece.color != destination.piece.color
+
+    # make the destination_spot.piece = piece
+    destination.piece = current_piece
+  end
+
+  # captures the enemy piece at the given position
+  def capture_at(x, y)
+    # remove the enemy piece from the spot
+    # mark it(piece) as captured
+    # place your piece on the spot
+  end
+
+  # checks if the spot is valid as per board boundaries
+  def valid_spot?(x, y)
+    board_size = 8
+    x >= 0 && x < board_size && y >= 0 && y < board_size
+  end
+
+  # checks if the two pieces are enemies or friends
+  def enemy_piece?(current_piece, piece_to_check)
+    current_piece.color != piece_to_check.color
+  end
+
+  # finds the spot in which given piece is inside
+  def find_spot(piece, spot_array)
+    spot_array.each do |row|
+      row.each do |spot|
+        return spot if spot.piece === piece
+      end
+    end
   end
 end
 
