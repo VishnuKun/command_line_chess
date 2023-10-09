ad9b1110162de899a0f01d078589008d03c67d60
Vishnu <vishnukun954AV@protonmail.com>
2023-10-09 19:56:27 +0530

    Refactored

--- spec/chessboard_spec.rb
+++ spec/chessboard_spec.rb
@@ -7,31 +7,29 @@ require_relative '../lib/chessboard'
 require_relative 'spec_helper'
 
 describe Chessboard do
-
-  describe "#move_piece" do 
+  describe '#move_piece' do
     subject(:chess_board) { described_class.new }
     let(:pawn) { Piece.create_piece(-1) }
-    
-    context "when pawn is at (1, 0) on an empty board" do
-      before do 
-        chess_board.add_piece(pawn,1, 0)
+    context 'when pawn is at (1, 0) on an empty board' do
+      before do
+        chess_board.add_piece(pawn, 1, 0)
       end
-      it "moves pawn from (1, 0) to (3, 0)" do
+      it 'moves pawn from (1, 0) to (3, 0)' do
         chess_board.move_piece(pawn, 3, 0)
         expect(chess_board.piece_at(3, 0)).to eq(pawn)
       end
     end
   end
 
-  describe "#remove_piece" do
+  describe '#remove_piece' do
     subject(:chess_board) { described_class.new }
-    let(:rook) { Piece.create_piece(-4)}
+    let(:rook) { Piece.create_piece(-4) }
 
-    context "when rook is at (0, 0)" do
-      before do 
+    context 'when rook is at (0, 0)' do
+      before do
         chess_board.add_piece(rook, 0, 0)
       end
-      it "removes rook from (0, 0)" do 
+      it 'removes rook from (0, 0)' do
         chess_board.remove_piece(0, 0)
         expect(chess_board.piece_at(0, 0)).to be_nil
       end
