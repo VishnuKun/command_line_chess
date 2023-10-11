1a08a37e339504a41f8e69af502d93a9d30e0f93
Vishnu <vishnukun954AV@protonmail.com>
2023-10-11 14:01:55 +0530

    Adjusted as per changes in code

--- spec/spot_spec.rb
+++ spec/spot_spec.rb
@@ -7,34 +7,22 @@ require 'pieces'
 require 'spec_helper'
 
 describe Spot do
-  describe '#is_empty?' do
+  describe '#empty?' do
     context 'when invoked on an empty spot' do
       subject(:empty_spot) { described_class.new }
       it 'returns true' do
-        expect(empty_spot.is_empty?).to eq(true)
+        expect(empty_spot.empty?).to eq(true)
       end
     end
 
     context 'when invoked on an filled spot' do
+      subject(:spot) { described_class.new }
+      let(:piece) { Piece.new(-1) }
       before do
-        subject(:piece) { Piece.new(-1) }
-        subject(:filled_spot) { described_class.new(piece) }
+        spot.piece = piece
       end
       it 'returns true' do
-        expect(filled_spot.is_empty?).to eq(false)
-      end
-    end
-  end
-
-  describe '#remove_piece' do
-    context 'when invoked on spot with a piece' do
-      subject(:piece) { Piece.new(1) }
-      subject(:spot_remove) { described_class.new(piece) }
-      before do
-        spot_remove.remove_piece
-      end
-      it 'removes the piece from the spot' do
-        expect(spot.empty).to eq(true)
+        expect(spot.empty?).to eq(false)
       end
     end
   end
