27ab0654987a0d02e893570c46aa9c15fc2904c0
Vishnu <vishnukun954AV@protonmail.com>
2023-10-18 11:58:30 +0530

    Checking valid moves of each moves directly in the can_attack? method now

--- lib/king_module.rb
+++ lib/king_module.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # lib/king_module.rb
 # contains methods for the King class
 module KingModule
@@ -16,6 +18,10 @@ module KingModule
       [0, 1] # up
     ]
 
+    # insert castling moves if possible
+    moves << [spot.row, spot.column - 2] if can_castle_to_left?(spot, board) # left castling move
+    moves << [spot.row, spot.column + 2] if can_castle_to_right?(spot, board) # left castling move
+
     # Check all directions for valid moves
     directions.each do |direction|
       row = spot.row + direction[0]
@@ -31,18 +37,13 @@ module KingModule
       # Check if the move puts the king in check
       moves << [row, column] unless in_check?(self, temp_board, row, column)
     end
-    # insert castling moves if possible
-    # left castling move
-    moves << [row, column - 2] if can_castle_to_left?(spot, board)
-    # right castling move
-    moves << [row, column + 2] if can_castle_to_right?(spot, board)
     moves
   end
 
   # checks if the move is valid for king
   def valid_king_move?(king, board, row, column)
-    if (board[row][column].empty? || enemy_piece?(king,
-                                                  board[row][column].piece)) && !in_check?(king, board, row, column)
+    # check if the spot is empty or it contains enemy piece
+    if board[row][column].empty? || enemy_piece?(king, board[row][column].piece)
       true
     else
       false
@@ -53,17 +54,11 @@ module KingModule
   def in_check?(king, board, row, column)
     # Iterate through all the enemy pieces on the board
     enemy_pieces = find_enemy_pieces(king, board)
-    king_position = [row, column]
 
     enemy_pieces.any? do |spot|
       enemy = spot.piece
-
-      # Calculate the difference between the enemy piece's position and the king's position
-      dx = (king_position[0] - spot.row).abs
-      dy = (king_position[1] - spot.column).abs
-
       # Check if the enemy piece can attack the king's current position
-      can_attack?(enemy, dx, dy)
+      can_attack?(enemy, board, row, column)
     end
   end
 
@@ -74,25 +69,19 @@ module KingModule
     end
   end
 
-  # check if the piece can attack the piece at given position
-  def can_attack?(enemy, dx, dy)
+  # check if the piece can attack the piece at the given position
+  def can_attack?(enemy, _board, row, column)
     case enemy
     when Pawn
-      # For a pawn, it can attack if the horizontal and vertical differences are both equal to 1.
-      dx.abs == 1 && dy.abs == 1
+      return true if enemy.valid_moves(_board).include?([row, column])
     when Knight
-      # For a knight, it can attack if the absolute values of dx and dy are either (2, 1) or (1, 2).
-      (dx.abs == 2 && dy.abs == 1) || (dx.abs == 1 && dy.abs == 2)
+      return true if enemy.valid_moves(_board).include?([row, column])
     when Bishop
-      # For a bishop, it can attack if the horizontal and vertical differences are equal, meaning diagonal movement.
-      dx.abs == dy.abs
+      return true if enemy.valid_moves(_board).include?([row, column])
     when Rook
-      # For a rook, it can attack if either the horizontal (dx) or vertical (dy) difference is zero.
-      dx.zero? || dy.zero?
+      return true if enemy.valid_moves(_board).include?([row, column])
     when Queen
-      # For a queen, it can attack if the horizontal and vertical differences are equal (diagonal movement)
-      # or if either of them is zero (straight-line movement).
-      dx.zero? || dy.zero? || dx.abs == dy.abs
+      return true if enemy.valid_moves(_board).include?([row, column])
     else
       false
     end
